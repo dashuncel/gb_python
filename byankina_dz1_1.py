1d20b0656440f88b357895a7aded2ee77e594d2b
dashuncel <korovkinada@ya.ru>
2021-04-07 10:47:35 +0800

    added comment

--- byankina_dz1_1.py
+++ byankina_dz1_1.py
@@ -9,16 +9,20 @@
 time_stamp = ''
 
 def get_year(digit):
-    return [ digit // 365, digit % 365]
+    #на вход даем количество дней
+    return [digit // 365, digit % 365]
 
 def get_minutes(digit):
-    return [ digit // 60, digit % 60]
+    # на вход даем количество секунд
+    return [digit // 60, digit % 60]
 
 def get_days(digit):
+    # на вход даем количество часов
     return [digit // 24, digit % 24]
 
 def get_hours(digit):
-    return [ digit // 60, digit % 60]
+    # на вход даем количество минут
+    return [digit // 60, digit % 60]
 
 while True:
     time_stamp = input("Введите время в секундах: ")
@@ -31,5 +35,5 @@ hours = get_hours(minutes[0])
 days = get_days(hours[0])
 years = get_days(days[0])
 
-print('В {0} секундах {1}Y {2}D {3}H:{4}M:{5}S'.format(time_stamp, years[0], years[1], days[1], hours[1], minutes[1]))
+print('В {0} секундах {1} years, {2} days, {3} hours, {4} min, {5} sec'.format(time_stamp, years[0], years[1], days[1], hours[1], minutes[1]))
 
